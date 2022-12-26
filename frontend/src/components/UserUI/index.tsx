4e81ac1a9f804ef62ac475eddd2c70ecf46f15bf
TomasOsiecki <tomasosiecki998@gmail.com>
2022-12-26 18:05:06 -0300

    Adds changes for code review
    Co-authored-by: Valentino Cerutti <Micrograx@users.noreply.github.com>

--- frontend/src/components/UserUI/index.tsx
+++ frontend/src/components/UserUI/index.tsx
@@ -1,16 +1,14 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 import { Container, Navbar, Nav, Button, Modal, Form, Table } from "react-bootstrap";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { UserEndpoints, ObsState, UtxoInfo } from "src/contractEndpoints/user";
 import { mkStartParams } from "src/contractEndpoints/parameters";
-import { CIP30WalletWrapper } from "cardano-pab-client";
 
 // Main component for the UserUI. It includes all the other components.
 function UserUI() {
   const [currentContractState, setCurrentContractState] = useState<ObsState>([])
   const [contractEndpoints, setContractEndpoints] = useState<UserEndpoints>(new UserEndpoints([]));
-  const [walletWrapper, setWalletWrapper] = useState<CIP30WalletWrapper>(undefined)
   const [isConnected, setIsConnected] = useState(false);
   const [showStartModal, setShowStartModal] = useState(false);
 
@@ -23,35 +21,33 @@ function UserUI() {
           setCurrentContractState={setCurrentContractState}
           contractEndpoints={contractEndpoints}
           setIsConnected={setIsConnected}
-          setWalletWrapper={setWalletWrapper}
+          setContractEndpoints={setContractEndpoints}
         />
       </Navbar>
       <br></br>
       <Button
-          style={{ marginRight: "20px" }}
-          variant="primary"
-          size="lg"
-          onClick={handleShow}
-          disabled={!isConnected}
+        style={{ marginRight: "20px" }}
+        variant="primary"
+        size="lg"
+        onClick={handleShow}
+        disabled={!isConnected}
       >
         Start new Escrow
       </Button>
       <Start
-        show={showStartModal}
-        handleShow={handleShow}
+        showStartModal={showStartModal}
         setShowStartModal={setShowStartModal}
         contractEndpoints={contractEndpoints}
-        walletWrapper={walletWrapper}
-        />
+      />
 
       <Button
-          style={{ marginRight: "20px" }}
-          variant="primary"
-          size="lg"
-          disabled={!isConnected}
-          onClick={async () =>
-            console.log("Cancel")
-          }
+        style={{ marginRight: "20px" }}
+        variant="primary"
+        size="lg"
+        disabled={!isConnected}
+        onClick={async () =>
+          console.log("Cancel")
+        }
       >
         Cancel an Escrow
       </Button>
@@ -67,8 +63,14 @@ function UserUI() {
   )
 }
 
+type ConnectProps = {
+  setCurrentContractState: React.Dispatch<React.SetStateAction<ObsState>>
+  contractEndpoints: UserEndpoints
+  setIsConnected: React.Dispatch<React.SetStateAction<boolean>>
+  setContractEndpoints: React.Dispatch<React.SetStateAction<UserEndpoints>>
+};
 // Connect component that handles the wallet and endpoint connection.
-const Connect = ({ setCurrentContractState, contractEndpoints, setIsConnected, setWalletWrapper }) => {
+const Connect = ({ setCurrentContractState, contractEndpoints, setIsConnected, setContractEndpoints }: ConnectProps) => {
   const [selectedWallet, setSelectedWallet] = useState("nami");
   return (
     <Navbar.Collapse className="justify-content-end">
@@ -88,17 +90,17 @@ const Connect = ({ setCurrentContractState, contractEndpoints, setIsConnected, s
           <option value="eternl">Eternl</option>
         </select>
         <Button
-            style={{ marginRight: "16px" }}
-            variant="success"
-            size="sm"
-            onClick={async e => {
-                console.log("Connecting")
-                contractEndpoints.connect(selectedWallet)
-                  .then(setIsConnected(true))
-                  .then(console.log("Connected"))
-                  .then(setWalletWrapper)
-              }
-            }
+          style={{ marginRight: "16px" }}
+          variant="success"
+          size="sm"
+          onClick={async e => {
+            console.log("Connecting")
+            const ce = await contractEndpoints.connect(selectedWallet)
+            setIsConnected(true)
+            console.log("Connected")
+            setContractEndpoints(ce)
+          }
+          }
         >
           Connect Wallet
         </Button>
@@ -115,26 +117,31 @@ const Reload = () => {
   )
 }
 
+type StartProps = {
+  showStartModal: boolean
+  setShowStartModal: React.Dispatch<React.SetStateAction<boolean>>
+  contractEndpoints: UserEndpoints
+};
 // Component that displays the form for starting a new Escrow.
-const Start = ({show, handleShow, setShowStartModal, contractEndpoints, walletWrapper}) => {
+const Start = ({ showStartModal, setShowStartModal, contractEndpoints }: StartProps) => {
 
   const handleClose = e => {
     setShowStartModal(false)
     e.preventDefault()
     const formData = new FormData(e.target),
-          recAddr = formData.get("recAddr") as string,
-          sendAsset = formData.get("sendAsset") as string,
-          sendAmount = parseInt(formData.get("sendAmount") as string),
-          recAsset = formData.get("recAsset") as string,
-          recAmount = parseInt(formData.get("recAmount") as string)
+      recAddr = formData.get("recAddr") as string,
+      sendAsset = formData.get("sendAsset") as string,
+      sendAmount = parseInt(formData.get("sendAmount") as string),
+      recAsset = formData.get("recAsset") as string,
+      recAmount = parseInt(formData.get("recAmount") as string)
 
     mkStartParams(recAddr, sendAsset, sendAmount, recAsset, recAmount)
-      .then(sp => contractEndpoints.start(sp,walletWrapper))
+      .then(sp => contractEndpoints.start(sp))
 
   }
-    return (
+  return (
     <>
-    <Modal show={show}>
+      <Modal show={showStartModal}>
         <Modal.Header closeButton>
           <Modal.Title>Start new Escrow</Modal.Title>
         </Modal.Header>
@@ -150,42 +157,42 @@ const Start = ({show, handleShow, setShowStartModal, contractEndpoints, walletWr
               <br></br>
               <Row>
                 <Col>
-                    <Form.Label>Send AssetClass</Form.Label>
-                    <Form.Control
-                        name="sendAsset"
-                        type="text"
-                        placeholder="Asset"
-                    />
+                  <Form.Label>Send AssetClass</Form.Label>
+                  <Form.Control
+                    name="sendAsset"
+                    type="text"
+                    placeholder="Asset"
+                  />
                 </Col>
                 <Col>
-                    <Form.Label>Send Amount</Form.Label>
-                    <Form.Control
-                        name="sendAmount"
-                        type="number"
-                        placeholder="Amount"
-                    />
+                  <Form.Label>Send Amount</Form.Label>
+                  <Form.Control
+                    name="sendAmount"
+                    type="number"
+                    placeholder="Amount"
+                  />
                 </Col>
               </Row>
               <br></br>
               <Row>
                 <Col>
-                    <Form.Label>Receive AssetClass</Form.Label>
-                    <Form.Control
-                        name="recAsset"
-                        placeholder="Asset"
-                    />
+                  <Form.Label>Receive AssetClass</Form.Label>
+                  <Form.Control
+                    name="recAsset"
+                    placeholder="Asset"
+                  />
                 </Col>
                 <Col>
-                    <Form.Label>Receive Amount</Form.Label>
-                    <Form.Control
-                        name="recAmount"
-                        type="number"
-                        placeholder="Amount"
-                    />
+                  <Form.Label>Receive Amount</Form.Label>
+                  <Form.Control
+                    name="recAmount"
+                    type="number"
+                    placeholder="Amount"
+                  />
                 </Col>
               </Row>
               <Button variant="primary" type="submit">
-              Submit
+                Submit
               </Button>
             </Form.Group>
           </Form>
@@ -220,51 +227,53 @@ const Resolve = () => {
     </>
   )
 }
-
+type ContractInformationProps = {
+  currentContractState: ObsState
+}
 // Component that displays the started escrows in a table.
-const ContractInformation = ( { currentContractState }  ) => {
+const ContractInformation = ({ currentContractState }: ContractInformationProps) => {
   return (
     <div
       style={{
-        textAlign:"center"
+        textAlign: "center"
       }}
     >
-    <Table striped bordered hover className="align-middle">
-      <thead>
-        <tr>
-          <th>Sender Address</th>
-          <th>Send Amount</th>
-          <th>Send Asset</th>
-          <th>Receive Amount</th>
-          <th>Receive Asset</th>
-          <th>Resolve</th>
-        </tr>
-      </thead>
-      <tbody>
-        {currentContractState.map((elem: UtxoInfo, index) => (
-          <tr key={index}>
-            <td> {elem.SenderAddr} </td>
-            <td> {elem.SendAmount} </td>
-            <td> {elem.SendAsset} </td>
-            <td> {elem.ReceiveAmount} </td>
-            <td> {elem.ReceiveAsset} </td>
-            <td >
+      <Table striped bordered hover className="align-middle">
+        <thead>
+          <tr>
+            <th>Sender Address</th>
+            <th>Send Amount</th>
+            <th>Send Asset</th>
+            <th>Receive Amount</th>
+            <th>Receive Asset</th>
+            <th>Resolve</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentContractState.map((elem: UtxoInfo, index) => (
+            <tr key={index}>
+              <td> {elem.SenderAddr} </td>
+              <td> {elem.SendAmount} </td>
+              <td> {elem.SendAsset} </td>
+              <td> {elem.ReceiveAmount} </td>
+              <td> {elem.ReceiveAsset} </td>
+              <td >
                 <Button
-                    style={{ marginRight: "16px" }}
-                    variant="success"
-                    size="sm"
-                    onClick={async e => {
-                        console.log("Resolving")
-                        console.log(elem)
-                    }
+                  style={{ marginRight: "16px" }}
+                  variant="success"
+                  size="sm"
+                  onClick={async e => {
+                    console.log("Resolving")
+                    console.log(elem)
+                  }
                   }
                 > Resolve
                 </Button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </Table>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
 
   )
--- frontend/src/contractEndpoints/user.ts
+++ frontend/src/contractEndpoints/user.ts
@@ -4,131 +4,126 @@ import { StartParams, mkWalletAddressFromString } from "./parameters";
  * The representation of the contract Utxo state
  */
 export type UtxoInfo = {
-    SenderAddr: String;
-    SendAsset: String;
-    SendAmount: Number;
-    ReceiveAsset: String;
-    ReceiveAmount: Number;
+  SenderAddr: String;
+  SendAsset: String;
+  SendAmount: Number;
+  ReceiveAsset: String;
+  ReceiveAmount: Number;
 }
 
 export type ObsState = UtxoInfo[]
 
 export class UserEndpoints {
-    contractState: ObsState = undefined
-    endpoints: ContractEndpoints = undefined
-    wallet: CIP30WalletWrapper = undefined
+  contractState: ObsState = undefined
+  endpoints: ContractEndpoints = undefined
+  wallet: CIP30WalletWrapper = undefined
 
-    constructor(
-        contractState : ObsState,
-    ) {
-      this.contractState = contractState
-    }
+  constructor(
+    contractState: ObsState,
+  ) {
+    this.contractState = contractState
+  }
 
-    public async connect(walletName): Promise<CIP30WalletWrapper> {
-        const {
-            getWalletInitialAPI,
-            CIP30WalletWrapper,
-            PABApi,
-            ContractEndpoints
-          } = await import("cardano-pab-client");
+  public async connect(walletName): Promise<UserEndpoints> {
+    const {
+      getWalletInitialAPI,
+      CIP30WalletWrapper,
+      PABApi,
+      ContractEndpoints
+    } = await import("cardano-pab-client");
 
-        let walletInitialAPI = getWalletInitialAPI(window, walletName);
+    let walletInitialAPI = getWalletInitialAPI(window, walletName);
 
-        // this will ask the user to give to this dApp access to their wallet methods
-        const walletInjectedFromBrowser = await walletInitialAPI.enable();
+    // this will ask the user to give to this dApp access to their wallet methods
+    const walletInjectedFromBrowser = await walletInitialAPI.enable();
 
-        // then we can initialize the CIP30WalletWrapper class of the library
-        this.wallet = await CIP30WalletWrapper.init(walletInjectedFromBrowser);
-        const addrs = await this.wallet.getUsedAddresses()
-        const walletAddr = mkWalletAddressFromString(addrs[0])
-        console.log(`Connected Address:`)
-        console.log(walletAddr)
+    // then we can initialize the CIP30WalletWrapper class of the library
+    this.wallet = await CIP30WalletWrapper.init(walletInjectedFromBrowser);
+    const addrs = await this.wallet.getUsedAddresses()
+    const walletAddr = mkWalletAddressFromString(addrs[0])
+    console.log(`Connected Address:`)
+    console.log(walletAddr)
 
-        // Try to get unbalanced transaction from PAB
-        const walletId = await this.wallet.getWalletId();
-        const pabApi = new PABApi("http://localhost:9080/api");
+    // Try to get unbalanced transaction from PAB
+    const walletId = await this.wallet.getWalletId();
+    const pabApi = new PABApi(process.env.REACT_APP_PAB_URL);
 
-        this.endpoints = await ContractEndpoints.connect(
-            walletId,
-            { endpointTag: "Connect", params: walletAddr},
-            pabApi,
-        );
+    this.endpoints = await ContractEndpoints.connect(
+      walletId,
+      { endpointTag: "Connect", params: walletAddr },
+      pabApi,
+    );
 
-        return this.wallet
-    }
+    return this
+  }
 
-    public async start(sp: StartParams, wallet: CIP30WalletWrapper) {
-        const {
-            TxBudgetAPI,
-            PABApi,
-            ContractEndpoints,
-            Balancer,
-            getProtocolParamsFromBlockfrost,
-            succeeded
-        } = await import("cardano-pab-client");
-        console.log(`Start Params:`)
-        console.log(sp)
-        // Initialize Balancer
-        const protocolParams = await getProtocolParamsFromBlockfrost(
-            "https://cardano-preprod.blockfrost.io/api/v0",
-            "preprodOmMR1EVAuqrxGGXglYoVwY3CWwKhEFs0",
-        );
-        const balancer = await Balancer.init(protocolParams);
+  public async start(sp: StartParams) {
+    const {
+      TxBudgetAPI,
+      Balancer,
+      getProtocolParamsFromBlockfrost,
+      succeeded
+    } = await import("cardano-pab-client");
+    console.log(`Start Params:`)
+    console.log(sp)
+    // Initialize Balancer
+    const protocolParams = await getProtocolParamsFromBlockfrost(
+      process.env.REACT_APP_BLOCKFROST_URL,
+      process.env.REACT_APP_BLOCKFROST_API_KEY,
+    );
+    const balancer = await Balancer.init(protocolParams);
 
-        // Try to get unbalanced transaction from PAB
-        const walletId = await wallet.getWalletId();
-        const pabApi = new PABApi("http://localhost:9080/api");
+    // Try to get unbalanced transaction from PAB
+    const pabResponse = await this.endpoints.doOperation(
+      { endpointTag: "start", params: sp }
+    );
 
-        const pabResponse = await this.endpoints.doOperation(
-            { endpointTag: "start", params: sp }
-        );
+    if (!succeeded(pabResponse)) {
+      alert(
+        `Didn't got the unbalanced transaction from the PAB. Error: ${pabResponse.error}`
+      );
+    } else {
+      // the pab yielded the unbalanced transaction. balance, sign and submit it.
+      const etx = pabResponse.value;
+      console.log(`Unbalanced tx:`);
+      console.log(etx)
+      const walletInfo = await this.wallet.getWalletInfo();
+      const txBudgetApi = new TxBudgetAPI({
+        baseUrl: process.env.REACT_APP_ESTIMATOR_URL,
+        timeout: 10000,
+      });
 
-        if (!succeeded(pabResponse)) {
-            alert(
-            `Didn't got the unbalanced transaction from the PAB. Error: ${pabResponse.error}`
-            );
-        } else {
-            // the pab yielded the unbalanced transaction. balance, sign and submit it.
-            const etx = pabResponse.value;
-            console.log(`Unbalanced tx:`);
-            console.log(etx)
-            const walletInfo = await wallet.getWalletInfo();
-            const txBudgetApi = new TxBudgetAPI({
-                baseUrl: "http://localhost:3001",
-                timeout: 10000,
-            });
-
-            const fullyBalancedTx = await balancer.fullBalanceTx(
-                etx,
-                walletInfo,
-                // configuration for the balanceTx and rebalanceTx methods which are interally
-                // used by this method
-                { feeUpperBound: 1000000, mergeSignerOutputs: false },
-                // a high-order function that exposes the balanced tx and the inputs info so to
-                // calculate the executions units, which are then set in the transaction and
-                // goes to the rebalancing step
-                async (balancedTx, inputsInfo) => {
-                  const txBudgetResponse = await txBudgetApi.estimate(balancedTx, inputsInfo);
-                  if (succeeded(txBudgetResponse)) {
-                      const units = txBudgetResponse.value;
-                      return units;
-                  } else {
-                      console.log("BALANCER FAILED")
-                      console.log(txBudgetResponse.error)
-                      return []
-                  }
-                }
-            );
-            // print to the console the fully balanced tx cbor for debugging purposes
-            console.log(`Balanced tx: ${fullyBalancedTx}`);
-            // now that the transaction is balanced, sign and submit it with the wallet
-            const response = await wallet.signAndSubmit(fullyBalancedTx);
-            if (succeeded(response)) {
-                const txHash = response.value;
-                alert(`Start suceeded. Tx hash: ${txHash}`);
-            } else {
-                alert(`Start failed when trying to submit it. Error: ${response.error}`);
-            }
+      const fullyBalancedTx = await balancer.fullBalanceTx(
+        etx,
+        walletInfo,
+        // configuration for the balanceTx and rebalanceTx methods which are interally
+        // used by this method
+        { feeUpperBound: 1000000, mergeSignerOutputs: false },
+        // a high-order function that exposes the balanced tx and the inputs info so to
+        // calculate the executions units, which are then set in the transaction and
+        // goes to the rebalancing step
+        async (balancedTx, inputsInfo) => {
+          const txBudgetResponse = await txBudgetApi.estimate(balancedTx, inputsInfo);
+          if (succeeded(txBudgetResponse)) {
+            const units = txBudgetResponse.value;
+            return units;
+          } else {
+            console.log("BALANCER FAILED")
+            console.log(txBudgetResponse.error)
+            return []
+          }
         }
+      );
+      // print to the console the fully balanced tx cbor for debugging purposes
+      console.log(`Balanced tx: ${fullyBalancedTx}`);
+      // now that the transaction is balanced, sign and submit it with the wallet
+      const response = await this.wallet.signAndSubmit(fullyBalancedTx);
+      if (succeeded(response)) {
+        const txHash = response.value;
+        alert(`Start suceeded. Tx hash: ${txHash}`);
+      } else {
+        alert(`Start failed when trying to submit it. Error: ${response.error}`);
+      }
     }
+  }
 }
